352196597b89c543d95d29fc8e190ffd09c47605
nikkolasg <nikkolasg@gmail.com>
2018-02-02 21:40:33 +0100

    some proto empty files

---
+++ proto/protocol.go
@@ -0,0 +1,10 @@
+package proto
+
+// Protocol is the core of dsign. It runs the necessary sub protocol (dkg / dss)
+// with the right parameters to get a dsign-ature.
+type Protocol struct {
+}
+
+func NewProtocol() *Protocol {
+
+}
